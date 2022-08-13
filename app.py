81b592d7994b370d9d350a7c95cc96faac95c27a
N Jaya Sankara Datta <jayasankaradatta5857@gmail.com>
2022-08-13 20:20:48 +0530

    Added PostgreSQL variable and psycopg2

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from flask import Flask
 from flask_restful import Api
 #from flask_jwt import JWT
 
-
-from db import db 
+ 
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -16,8 +15,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///data.db")
 #the get methon taking two values if "DATABASE_URL" is not found in environment variables it considers sqlite database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
@@ -38,5 +36,6 @@ api.add_resource(Store, "/store/<name>")
 api.add_resource(StoreList, "/stores")
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)  # important to mention debug=True
